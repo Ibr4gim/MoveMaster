4c030a20a7fd553cf7bc1e5c3cfb34254963cddb
ibragim <ibragimismailov.12.12.7@gmail.com>
2025-02-22 18:20:41 +0600

    Добавил шахматную доску и исправил иконку

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+
 import CustomButton from "./components/Button";
 import Header from "./components/Header";
+import ChessBoard from "./components/ChessBoard";
 
 const App = () => {
   return (
     <Container className="d-flex flex-column align-items-center p-4">
       <Header />
       <CustomButton text="Click me" />
+      <ChessBoard />
     </Container>
   );
 };
