94131d2d2ea1ed67df227bf91f436d4c58022305
ibragim <ibragimismailov.12.12.7@gmail.com>
2025-02-24 18:24:12 +0600

    added cards

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import { Container } from "react-bootstrap";
 import CustomButton from "./components/Button";
 import Header from "./components/Header";
 import ChessGame from "./components/ChessGame";
-import InfoCards from "./components/infoCards";
-import InfoCards from "./components/infoCards";
+// import InfoCards from "./components/infoCards";
+// import InfoCards from "./components/infoCards";
 
 const App = () => {
   return (
     <Container className="d-flex flex-column align-items-center p-4 ">
       <Header />
-      {/* <CustomButton text="Click me" /> */}
-      {/* <ChessGame /> */}
-      <InfoCards />
+      <CustomButton text="Click me" />
+      <ChessGame />
+      {/* <InfoCards /> */}
     </Container>
   );
 };
